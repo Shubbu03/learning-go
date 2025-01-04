a84b57f27bee6a88f8bfd0457d2d7cfb8768f12b
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-01-04 12:11:53 +0530

    Add Hellos function to greetings module and implement tests

--- custom-module/greetings/greetings.go
+++ custom-module/greetings/greetings.go
@@ -41,3 +41,19 @@ func randomFormat() string {
 
     return formats[rand.Intn(len(formats))]
 }
+
+func Hellos(names []string) (map[string]string,error){
+	messages := make(map[string]string)
+    // Loop through the received slice of names, calling
+    // the Hello function to get a message for each name.
+    for _, name := range names {
+        message, err := Hi(name)
+        if err != nil {
+            return nil, err
+        }
+        // In the map, associate the retrieved message with
+        // the name.
+        messages[name] = message
+    }
+    return messages, nil
+}
--- custom-module/hello/hello.go
+++ custom-module/hello/hello.go
@@ -11,7 +11,9 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hi("demo")
+    names := []string{"shubbu","dhiru","ankish"}
+
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
